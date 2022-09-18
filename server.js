a7d3559a010449e640b2f4506a47d689c9e18a91
Sachi Goto <70562492+SachiGoto@users.noreply.github.com>
2022-09-17 23:04:12 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,55 @@
+const express = require('express')
+const PORT = 8000;
+// request express// 
+
+const rappers ={
+   '21 savage':{
+
+    'age': 29,
+    'birthname' :'Sheyaa Bin Abraham - Joseph',
+    'birthlocaion': 'London, England'
+
+
+   } ,
+   'chance the rapper':{
+
+    'age': 29,
+    'birthname' :'Chancelor Bennet',
+    'birthlocaion': 'Chicago, Illinois'
+
+
+   } ,
+   'dylan':{
+
+    'age': 29,
+    'birthname' :'Dylan',
+    'birthlocaion': 'Chicago, Illinois'
+
+
+   } 
+}
+
+const app = express();
+// create an variable that holds express 
+
+app.get('/', (request, response)=>{
+   response.sendFile(__dirname + '/index.html')
+   // __dirname - start from whre server.js is located and find the html 
+})
+
+app.get('/api/:rapperName', (request, response)=>{
+    // respond back json
+    const rappersName = request.params.rapperName.toLowerCase();
+    console.log(request.params.rapperName)
+if(rappers[rappersName]){
+    response.json(rappers[rappersName]);
+
+}else{
+    response.json(rappers['dylan']);
+}
+    //  response.json(rappers);
+})
+
+app.listen(PORT, ()=>{
+    console.log(`The server is running on ${PORT}!`)
+})
